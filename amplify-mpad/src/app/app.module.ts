6df67ae1d73041614bba91d02d75fa68e0b338d5
m-pad-studios <mpadinds@gmail.com>
2020-06-04 23:29:07 -0700

    initial commit

---
+++ amplify-mpad/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AmplifyAngularModule, AmplifyIonicModule, AmplifyService } from 'aws-amplify-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AmplifyAngularModule,
+    AmplifyIonicModule
+  ],
+  providers: [
+    AmplifyService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
